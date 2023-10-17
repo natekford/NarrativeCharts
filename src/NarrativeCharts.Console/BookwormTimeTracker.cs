85ff0dc210fd2614b435cf3c7460731c29e1d483
Advorange <advorange@live.com>
2023-10-17 16:38:43 -0700

    Added in a class to keep track of time and handle stuff relative to bells for bookworm.

---
+++ src/NarrativeCharts.Console/BookwormTimeTracker.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections.Immutable;
+
+namespace NarrativeCharts.Console;
+
+public class BookwormTimeTracker
+{
+	private const int HOURS_PER_DAY = 24;
+
+	private readonly ImmutableArray<int> _HourToBellMap;
+	public ImmutableArray<int> Bells { get; }
+	public int CurrentBell => _HourToBellMap[CurrentHour];
+	public int CurrentDay => CurrentTotalHours / HOURS_PER_DAY;
+	public int CurrentHour => CurrentTotalHours % HOURS_PER_DAY;
+	public int CurrentTotalHours { get; private set; }
+	public BookwormBellMover GoToCurrentDay { get; }
+	public BookwormBellMover GoToNextDay { get; }
+
+	public BookwormTimeTracker()
+	{
+		// source: https://w.atwiki.jp/booklove/pages/195.html#footnote_body_2
+		Bells = new int[]
+		{
+			0,
+			4,
+			7,
+			10,
+			12,
+			15,
+			17,
+			20,
+		}.ToImmutableArray();
+		GoToCurrentDay = new BookwormBellMover(this, dayDifference: 0);
+		GoToNextDay = new BookwormBellMover(this, dayDifference: 1);
+
+		var hourToBellMap = new int[HOURS_PER_DAY];
+		for (var hour = 0; hour < HOURS_PER_DAY; ++hour)
+		{
+			var bell = 0;
+			for (; bell < Bells.Length; ++bell)
+			{
+				if (Bells[bell] > hour)
+				{
+					break;
+				}
+			}
+			hourToBellMap[hour] = bell - 1;
+		}
+		_HourToBellMap = hourToBellMap.ToImmutableArray();
+	}
+
+	public void AddBell()
+	{
+		if (CurrentBell == Bells.Length - 1)
+		{
+			GoToStartOfNextDay();
+		}
+		else
+		{
+			GoToBellOfCurrentDay(CurrentBell + 1);
+		}
+	}
+
+	public void AddBells(int bells)
+	{
+		// cba to think of the logic to do this without a loop
+		for (var i = 0; i < bells; ++i)
+		{
+			AddBell();
+		}
+	}
+
+	public void AddDay()
+		=> CurrentTotalHours += HOURS_PER_DAY;
+
+	public void AddDays(int days)
+		=> CurrentTotalHours += HOURS_PER_DAY * days;
+
+	public void AddHour()
+		=> ++CurrentTotalHours;
+
+	public void GoToBellOfCurrentDay(int bell)
+	{
+		var desiredHour = Bells[bell];
+		var currentHour = CurrentTotalHours % HOURS_PER_DAY;
+		if (currentHour > desiredHour)
+		{
+			throw new InvalidOperationException("Not allowed to go back in time.");
+		}
+
+		CurrentTotalHours += desiredHour - currentHour;
+	}
+
+	public void GoToBellOfNextDay(int bell)
+	{
+		GoToStartOfNextDay();
+		CurrentTotalHours += Bells[bell];
+	}
+
+	public BookwormBellMover GoToDaysAhead(int days)
+		=> new(this, dayDifference: days);
+
+	public void GoToStartOfNextDay()
+		=> CurrentTotalHours += HOURS_PER_DAY - (CurrentTotalHours % HOURS_PER_DAY);
+
+	public readonly struct BookwormBellMover
+	{
+		private readonly int _DayDifference;
+		private readonly BookwormTimeTracker _Tracker;
+
+		public BookwormBellMover(BookwormTimeTracker tracker, int dayDifference)
+		{
+			_Tracker = tracker;
+			_DayDifference = dayDifference;
+		}
+
+		public void Bed()
+			=> Move(7);
+
+		public void Dinner()
+			=> Move(6);
+
+		public void EarlyMorning()
+			=> Move(1);
+
+		public void Lunch()
+			=> Move(4);
+
+		public void MarketClose()
+			=> Move(5);
+
+		public void Meetings()
+			=> Move(3);
+
+		public void Morning()
+			=> Move(2);
+
+		private void Move(int bell)
+		{
+			if (_DayDifference == 0)
+			{
+				_Tracker.GoToBellOfCurrentDay(bell);
+				return;
+			}
+
+			_Tracker.GoToBellOfNextDay(bell);
+			if (_DayDifference > 1)
+			{
+				_Tracker.AddDays(_DayDifference - 1);
+			}
+		}
+	}
+}
--- src/NarrativeCharts.Console/Program.cs
+++ src/NarrativeCharts.Console/Program.cs
@@ -9,85 +9,82 @@ public static class Program
 	private static void Main()
 	{
 		var generator = new NarrativeChartGenerator();
-		var hours = 0;
-		Point Point(double location) => new(hours, location);
+		var time = new BookwormTimeTracker();
+		Point Point(double location) => new(time.CurrentTotalHours, location);
 
 		// Prologue starts with Karstedt seeing Sylvester off
 		generator.AddScene(Point(Temple).With(Ferdinand, Karstedt, Myne, Sylvester));
-
 		// Time: Immediately after
-		++hours;
+		time.AddHour();
 		// Sylvester goes back to AD conf
 		generator.AddScene(Point(RoyalAcademy).With(Sylvester));
 		// Discussion about Myne
 		generator.AddScene(Point(Temple).With(Ferdinand, Karstedt));
+		// karstedt probably goes back to his house after the discussion
+		time.AddBell();
+		generator.AddScene(Point(KarstedtsHouse).With(Karstedt));
 
 		// Time: Next morning
-		hours += 12;
+		time.GoToNextDay.Morning();
 		// Karstedt interrogates these 2
 		generator.AddScene(Point(KnightsOrder).With(Bezewanst, Bindewald, Karstedt));
-
 		// Time: Dinner
-		hours += 12;
+		time.GoToCurrentDay.Dinner();
 		// Discussion about Myne's baptism
 		generator.AddScene(Point(KarstedtsHouse).With(Elvira, Ferdinand, Karstedt));
 
-		// Time: After dinner
-		hours += 12;
+		// Time: The next day
+		time.GoToNextDay.Morning();
 		// Karstest keeps interrogating these 2
 		generator.AddScene(Point(KnightsOrder).With(Bezewanst, Bindewald, Karstedt));
-
 		// Time: Not long after
-		hours += 3;
+		time.AddBell();
 		// Myne's health checkup
 		generator.AddScene(Point(Temple).With(Ferdinand, Karstedt, Myne));
-
 		// Time: Immediately after
-		++hours;
+		time.AddBell();
 		// Leaving Ferdinand's office and going back to Myne's room
 		generator.AddScene(Point(Temple).With(Damuel, Fran, Monika, Myne, Nicola, Rosina));
 
 		// Time: Next 3 days
-		hours += 72;
+		time.GoToDaysAhead(3).Morning();
 		// People in the temple getting ready for Myne going to the Noble's quarter
 		generator.AddScene(Point(Temple).With(Ella, Fran, Gil, Monika, Myne, Nicola, Rosina, Wilma));
 		// Cooks learning with Leise at Guildmaster's house (or italian restaurant?)
 		generator.AddScene(Point(ItalianRestaurant).With(Hugo, Leise, Todd));
-
 		// Time: Immediately after
-		++hours;
+		time.AddHour();
 		// Leaving the temple and going to the Noble's quarter
 		generator.AddScene(Point(TempleFrontEntrance).With(Ella, Ferdinand, Karstedt, Myne, Rosina));
-
 		// Time: Immediately after
-		++hours;
+		time.AddHour();
 		// Arriving at Karstedt's house
 		generator.AddScene(Point(KarstedtsHouse).With(Cornelius, Ella, Elvira, Ferdinand, Karstedt, Myne, Rosina));
 
 		// Karstedt and Cornelius commute to the Knight's Order every day so probably add that somehow?
 
 		// Time: Ferdinand checks on Myne every 2 days so probably stays like 6 hours each time?
-		hours += 6;
+		time.AddBells(2);
 		// Ferdinand going back to the temple
 		generator.AddScene(Point(Temple).With(Ferdinand));
 
 		for (var i = 0; i < 2; ++i)
 		{
-			hours += 42;
+			time.GoToDaysAhead(2).Meetings();
 			generator.AddScene(Point(KarstedtsHouse).With(Ferdinand));
-			hours += 6;
+			time.AddBells(2);
 			generator.AddScene(Point(Temple).With(Ferdinand));
 		}
 
 		// Time: On a day where Ferdinand isn't there?
-		hours += 24;
+		time.GoToNextDay.Meetings();
 		// Gilberta company comes to sell some rinsham to Myne
 		generator.AddScene(Point(KarstedtsHouse).With(Benno, Mark));
-		++hours;
+		time.AddBell();
 		generator.AddScene(Point(GilbertaCompany).With(Benno, Mark));
 
 		// Time: A day after the Gilberta company visits?
-		hours += 17;
+		time.GoToNextDay.Meetings();
 		// Ferdinand promises Myne book room key if she memorizes everyone's names before baptism
 		generator.AddScene(Point(KarstedtsHouse).With(Ferdinand));
 
@@ -98,16 +95,16 @@ public static class Program
 
 		for (var i = 0; i < 3; ++i)
 		{
-			hours += 6;
+			time.AddBells(2);
 			generator.AddScene(Point(Temple).With(Ferdinand));
-			hours += 42;
+			time.GoToDaysAhead(2).Meetings();
 			generator.AddScene(Point(KarstedtsHouse).With(Ferdinand));
 		}
-		hours += 6;
+		time.AddBells(2);
 		generator.AddScene(Point(Temple).With(Ferdinand));
 
 		// Time: The day before the baptism
-		hours += 18;
+		time.GoToNextDay.Meetings();
 		// Eckhart and Lemprecht come back home from the knight's barracks for Myne's baptism
 		generator.AddScene(Point(KarstedtsHouse).With(Eckhart, Lamprecht));
 
@@ -116,62 +113,57 @@ public static class Program
 		generator.AddScene(Point(Temple).With(Gil));
 		generator.AddScene(Point(GilbertaCompany).With(Benno, Lutz));
 
-		hours += 24;
+		time.GoToNextDay.Meetings();
 		// Everyone arrives for Myne's baptism (SS would add a bunch of characters)
 		generator.AddScene(Point(KarstedtsHouse).With(Ferdinand, Florencia, Sylvester, Wilfried));
-
 		// Time: Immediately after the baptism ceremony
-		++hours;
+		time.AddBell();
 		// Guard knights get introduced to Myne and Wilf makes Myne pass out
 		generator.AddScene(Point(KarstedtsHouse).With(Brigitte, Damuel));
-
 		// Time: After Myne wakes up and talks with Ferdinand/Karstedt
-		hours += 3;
+		time.AddBell();
 		generator.AddScene(Point(Temple).With(Ferdinand));
 		generator.AddScene(Point(Castle).With(Florencia, Sylvester, Wilfried));
 
 		// Time: "Ferdinand had told me to use the day after my baptism ceremony to rest."
-		hours += 20;
+		time.GoToNextDay.Morning();
 		// Lamprecht gives Myne a book and goes back the castle
 		generator.AddScene(Point(Castle).With(Lamprecht));
 
 		// Time: The next day
-		hours += 24;
+		time.GoToNextDay.Morning();
 		// "Karstedt and Cornelius had already headed to the Knight’s Order, so Elvira was the only one to see me off."
 		generator.AddScene(Point(KnightsOrder).With(Cornelius, Karstedt));
 		generator.AddScene(Point(Temple).With(Brigitte, Damuel, Ella, Myne, Rosina));
-
 		// Inaguration ceremony
-		hours += 3;
+		time.AddBell();
 		// Egmont gets crushed a bit
 		generator.AddScene(Point(Temple).With(Egmont));
 
 		// Time: "As I ate breakfast the next morning, Gil informed me ... meeting with the Gilberta Company later that day ..."
 		// "The Gilberta Company was due to arrive at third bell"
-		hours += 21;
+		time.GoToNextDay.Morning();
 		// Zahm introduced as Arno's replacement
 		generator.AddScene(Point(Temple).With(Benno, Lutz, Mark, Zahm));
-		hours += 3;
+		time.AddBell();
 		// Leaving after discussing printing/restaurant
 		generator.AddScene(Point(GilbertaCompany).With(Benno, Lutz, Mark));
 
 		// Time: "As the Starbind Ceremony approached," idk, a couple days later?
-		hours += 69;
+		time.GoToDaysAhead(3).Meetings();
 		// "Today was a day when Benno and Lutz were visiting from the Gilberta Company"
 		generator.AddScene(Point(Temple).With(Benno, Lutz));
-		hours += 3;
+		time.AddBell();
 		// Leaving after discussing starbinding ceremony
 		generator.AddScene(Point(GilbertaCompany).With(Benno, Lutz));
-
 		// Time: "And so, the day of the Starbind Ceremony arrived."
-		hours += 45;
+		time.GoToDaysAhead(2).Morning();
 		// "I thought about him, Lutz, and the kids who were about to head to the forest."
 		generator.AddScene(Point(LowerCityForest).With(Gil, Lutz));
 		// "my whole family had come to see me as the High Bishop."
 		generator.AddScene(Point(Temple).With(Effa, Gunther, Kamil, Tuuli));
-
 		// Time: lunch is between the lower city and noble starbindings
-		hours += 3;
+		time.AddBell();
 		generator.AddScene(Point(MynesHouse).With(Effa, Gunther, Kamil, Tuuli));
 		generator.AddScene(Point(Castle).With(Brigitte, Damuel, Ferdinand, Fran, Myne, Rosina));
 		// Myne gets introduced to her temp guard knights and some attendents
@@ -179,7 +171,7 @@ public static class Program
 		// gil and lutz probably return from the forest fairly soon
 		generator.AddScene(Point(Temple).With(Gil));
 		generator.AddScene(Point(GilbertaCompany).With(Lutz));
-		hours += 3;
+		time.AddBell();
 		// kids say good night to aub
 		generator.AddScene(Point(Castle).With(Charlotte, Melchior));
 		// myne gets changed into high bishop outfit
@@ -188,43 +180,39 @@ public static class Program
 		generator.AddScene(Point(Castle).With(Karstedt));
 
 		// Time: the next day
-		hours += 12;
+		time.GoToNextDay.Morning();
 		// "He sent his attendants back to the temple without him"
 		generator.AddScene(Point(Temple).With(Fran, Rosina));
 		// Time: "later that afternoon that my fever finally went down."
-		hours += 6;
+		time.GoToNextDay.MarketClose();
 		// "He put me onto his highbeast and we returned to the temple accompanied by Damuel and Brigitte, who followed on either side of us."
 		generator.AddScene(Point(Temple).With(Brigitte, Damuel, Ferdinand, Myne));
 
 		// Time: tomorrow
-		hours += 24;
+		time.GoToNextDay.Meetings();
 		// "Leon will be coming by tomorrow to get the natural yeast"
 		generator.AddScene(Point(Temple).With(Leon));
-		++hours;
+		time.AddHour();
 		generator.AddScene(Point(GilbertaCompany).With(Leon));
 
 		// Time: 3rd bell of the next day
-		hours += 15;
+		time.GoToNextDay.Meetings();
 		// sylvester arrives in the temple early on the day of the italian restaurant visit
 		generator.AddScene(Point(Temple).With(Cornelius, Eckhart, Karstedt, Sylvester));
-
-		++hours;
+		time.AddHour();
 		// rosina leaves early b/c she plays music
 		generator.AddScene(Point(ItalianRestaurant).With(Rosina));
-		hours += 2;
+		time.AddBell();
 		generator.AddScene(Point(ItalianRestaurant).With(Brigitte, Cornelius, Damuel, Eckhart, Ferdinand, Fran, Karstedt, Myne, Sylvester, Zahm));
 		generator.AddScene(Point(ItalianRestaurant).With(Benno, Freida, Gustav, Leon, Mark));
-
 		// Time: after lunch at the italian restaurant
-		hours += 2;
+		time.AddBell();
 		// hasse orphanage designed, fly over with highbeasts
 		generator.AddScene(Point(Hasse).With(Benno, Brigitte, Cornelius, Damuel, Eckhart, Ferdinand, Gustav, Karstedt, Mark, Myne, Sylvester));
-
-		hours += 3;
+		time.AddBell();
 		// hasse orphanage is built and they return to the restaurant
 		generator.AddScene(Point(ItalianRestaurant).With(Benno, Brigitte, Cornelius, Damuel, Eckhart, Ferdinand, Gustav, Karstedt, Mark, Myne, Sylvester));
-
-		++hours;
+		time.AddHour();
 		// ferdi hires todd as a chef to arrive in the tmple in 36 hours from now
 		// syl hires hugo for the castle
 		generator.AddScene(Point(OthmarCompany).With(Freida, Gustav));
@@ -236,109 +224,103 @@ public static class Program
 
 		// no transition between chapters so no idea the time difference between italian restaurant and donations chapter
 		// but hugo is in the castle kitched at that point, and we can probably assume he goes at the same time
-		// todd goes to ferdi's temple kitchen
-		hours += 36;
+		// todd goes to ferdi's temple kitchen at 2nd bell
+		time.GoToDaysAhead(2).Morning();
 		generator.AddScene(Point(Temple).With(Todd));
 		generator.AddScene(Point(Castle).With(Hugo));
 
 		// Time: during the day, idk, several days after the chefs arrive?
-		hours += 48;
+		time.GoToDaysAhead(2).Meetings();
 		// donation tea party with flor, elvira, and myne
 		// brigitte mentioned in next segment, so other guard knights probably also there
 		generator.AddScene(Point(Castle).With(Angelica, Brigitte, Cornelius, Damuel, Elvira, Myne));
-
-		hours += 3;
+		time.AddBell();
 		// elvira probably goes back after the tea party
 		generator.AddScene(Point(KarstedtsHouse).With(Elvira));
 
 		// Time: "I had been bedridden for two days since the tea party"
-		hours += 45;
+		time.GoToDaysAhead(2).Meetings();
 		// ferdi, elvira, flor visit sick myne
 		generator.AddScene(Point(Castle).With(Elvira, Ferdinand));
-		++hours;
+		time.AddBell();
 		// ferdi leaves after agreeing to play harspiel
 		generator.AddScene(Point(Temple).With(Ferdinand));
 
-		hours += 24;
+		time.GoToNextDay.Meetings();
 		generator.AddScene(Point(Castle).With(Ferdinand));
-
-		++hours;
+		time.AddHour();
 		// ferdi and myne go to some place to train magic
 		generator.AddScene(Point(KnightsOrder).With(Angelica, Brigitte, Cornelius, Damuel, Ferdinand, Myne));
-
-		hours += 3;
+		time.AddBells(2);
 		// myne blows up her feystone and reforms it then they go back to the castle
 		generator.AddScene(Point(Castle).With(Angelica, Brigitte, Cornelius, Damuel, Ferdinand, Myne));
-		++hours;
+		time.AddHour();
 		generator.AddScene(Point(Temple).With(Ferdinand));
 
-		hours += 21;
+		time.GoToNextDay.Meetings();
 		generator.AddScene(Point(Temple).With(Brigitte, Damuel, Myne));
 		generator.AddScene(Point(Temple).With(Benno, Lutz));
-
-		++hours;
+		time.AddBell();
 		generator.AddScene(Point(GilbertaCompany).With(Benno, Lutz));
 		// myne has dirk drain his mana with a taue fruit and cuts some trombes
 		generator.AddScene(Point(Temple).With(Delia, Dirk));
 
-		hours += 23;
+		time.GoToNextDay.Meetings();
 		generator.AddScene(Point(Temple).With(Johann, Lutz, Zack));
 		generator.AddScene(Point(LowerCityForest).With(Gil));
-
-		hours += 3;
+		time.AddBell();
 		generator.AddScene(Point(LowerCityWorkshops).With(Johann, Zack));
 		generator.AddScene(Point(GilbertaCompany).With(Lutz));
 		generator.AddScene(Point(Temple).With(Gil));
 
-		hours += 69;
+		time.GoToDaysAhead(3).Meetings();
 		// johann and zack return with their blueprints for the wax stencil machine
 		generator.AddScene(Point(Temple).With(Johann, Lutz, Zack));
-		++hours;
+		time.AddBell();
 		generator.AddScene(Point(LowerCityWorkshops).With(Johann, Zack));
 		generator.AddScene(Point(GilbertaCompany).With(Lutz));
 
-		hours += 23;
+		time.GoToNextDay.Meetings();
 		// tuuli gives myne a hairpin
 		generator.AddScene(Point(Temple).With(Lutz, Tuuli));
-		++hours;
+		time.AddBell();
 		generator.AddScene(Point(MynesHouse).With(Tuuli));
 		generator.AddScene(Point(GilbertaCompany).With(Lutz));
 
-		hours += 23;
+		time.GoToNextDay.Meetings();
 		generator.AddScene(Point(Temple).With(Lutz));
-		hours += 3;
+		time.AddBell();
 		generator.AddScene(Point(GilbertaCompany).With(Lutz));
 
 		// Time: 3 days later
-		hours += 69;
+		time.GoToDaysAhead(3).Meetings();
 		// elvira and lemprecht visit the temple to eat some of ella's food
 		generator.AddScene(Point(Temple).With(Elvira, Lamprecht));
-		hours += 3;
+		time.AddBell();
 		generator.AddScene(Point(KarstedtsHouse).With(Elvira));
 		generator.AddScene(Point(Castle).With(Lamprecht));
 
 		// Time: unknown, not the same day
-		hours += 21;
+		time.GoToNextDay.Meetings();
 		// lutz tells about johann and zack's progress
 		generator.AddScene(Point(Temple).With(Lutz));
-		hours += 3;
+		time.AddBell();
 		// myne goes to the castle to make her highbeast
 		generator.AddScene(Point(KnightsOrder).With(Brigitte, Damuel, Ferdinand, Myne));
-
-		hours += 6;
+		time.AddBell();
 		// they go back to the temple after myne creates lessy
 		generator.AddScene(Point(Temple).With(Brigitte, Damuel, Ferdinand, Myne));
-		// 13 days since concert was said to be "in a month"
 
 		// Time: "From there, I spent my days practicing ... "
 		// "It was the evening five days before Ferdinand’s concert."
-		hours += 15 + (23 * 24);
+		// 13 days since concert was said to be "in a month"
+		// so a 12 day timeskip at minimum
+		time.GoToDaysAhead(12).Meetings();
 		// lutz and smiths visit with a wax machine
 		generator.AddScene(Point(Temple).With(Lutz, Johann, Zack));
+		time.AddBell();
 
-		hours += 3;
-
-		System.Console.WriteLine(hours);
-		var dog = 1;
+		System.Console.WriteLine(time.CurrentTotalHours);
+		System.Console.ReadLine();
 	}
 }
