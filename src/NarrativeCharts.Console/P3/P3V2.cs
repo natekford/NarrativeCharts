0472518b13435bf8cb34c6bb170e8f3a5ca73758
Advorange <advorange@live.com>
2023-10-17 18:27:50 -0700

    Split each volume's narrative chart generator to its own file.

---
+++ src/NarrativeCharts.Console/P3/P3V2.cs
@@ -0,0 +1,17 @@
+﻿using NarrativeCharts.Models;
+
+using static NarrativeCharts.Console.Characters;
+using static NarrativeCharts.Console.Locations;
+
+namespace NarrativeCharts.Console.P3;
+
+public static class P3V2
+{
+	public static NarrativeChartGenerator Generate(BookwormTimeTracker time)
+	{
+		var p3v2 = new NarrativeChartGenerator();
+		Point Scene(double location) => new(time.CurrentTotalHours, location);
+
+		return p3v2;
+	}
+}
