204e66135bb6435f51ff9b2e5d34666f3363f511
Advorange <advorange@live.com>
2023-10-19 16:38:28 -0700

    reduced some code duplication

--- src/NarrativeCharts.ScottPlot/PlotUtils.cs
+++ src/NarrativeCharts.ScottPlot/PlotUtils.cs
@@ -1,4 +1,5 @@
 ﻿using ScottPlot;
+using ScottPlot.Renderable;
 
 using System.Collections.Concurrent;
 
@@ -13,40 +14,47 @@ public static class PlotUtils
 		var dupes = new ConcurrentDictionary<(double, double), int>();
 		foreach (var (character, points) in chart.Points.OrderBy(x => x.Value.Count))
 		{
-			var xs1 = new double[points.Count];
-			var ys1 = new double[points.Count];
+			var xs = new double[points.Count];
+			var ys = new double[points.Count];
 			for (var p = 0; p < points.Count; ++p)
 			{
 				var x = points.Values[p].Point.X;
 				var y = points.Values[p].Point.Y;
 				var shift = dupes.AddOrUpdate((x, y), _ => 1, (_, v) => ++v);
-				xs1[p] = x;
-				ys1[p] = y + (shift * 3);
+				xs[p] = x;
+				ys[p] = y + (shift * 3);
 			}
 
 			var color = System.Drawing.ColorTranslator.FromHtml(chart.Colors[character]);
-			plot.AddScatter(xs1, ys1, color: color, markerSize: 5, label: character);
+			plot.AddScatter(xs, ys, color: color, markerSize: 5, label: character);
 		}
 
 		// Y-Labels (locations)
-		{
-			var orderedLocs = chart.Locations.OrderBy(x => x.Value);
-			var locationPositions = orderedLocs.Select(x => (double)x.Value).ToArray();
-			var locations = orderedLocs.Select(x => x.Key).ToArray();
-
-			plot.YAxis.ManualTickPositions(locationPositions, locations);
-		}
-
+		plot.YAxis.SetTicks(chart.Locations, x => x.Value, x => x.Key);
 		// X-Labels (events/chapters)
-		{
-			var orderedEvents = chart.Events.OrderBy(x => x.Key);
-			var eventPositions = orderedEvents.Select(x => (double)x.Key).ToArray();
-			var events = orderedEvents.Select(x => x.Value.Name).ToArray();
-
-			plot.XAxis.ManualTickPositions(eventPositions, events);
-		}
+		plot.XAxis.SetTicks(chart.Events, x => x.Key, x => x.Value.Name);
 
 		plot.Legend();
 		plot.SaveFig(path);
+	}
+
+	private static void SetTicks<TSource>(
+		this Axis axis,
+		IReadOnlyCollection<TSource> source,
+		Func<TSource, double> getPosition,
+		Func<TSource, string> getLabel)
+	{
+		var positions = new double[source.Count];
+		var labels = new string[source.Count];
+
+		var i = 0;
+		foreach (var item in source.OrderBy(getPosition))
+		{
+			positions[i] = getPosition(item);
+			labels[i] = getLabel(item);
+			++i;
+		}
+
+		axis.ManualTickPositions(positions, labels);
 	}
 }
