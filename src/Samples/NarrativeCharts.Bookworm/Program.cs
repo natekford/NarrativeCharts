bf1cb77d1caa78ff40decf2cd30fb2bd120e380d
Advorange <advorange@live.com>
2023-10-19 16:13:19 -0700

    program now deals with collection of books instead of each one individually

--- src/Samples/NarrativeCharts.Bookworm/Program.cs
+++ src/Samples/NarrativeCharts.Bookworm/Program.cs
@@ -7,8 +7,11 @@ public static class Program
 	private static void Main()
 	{
 		var time = new BookwormTimeTracker();
-		var p3v1 = new P3V1(time).Create();
-		var p3v2 = new P3V2(time).Create();
+		var books = new[]
+		{
+			new P3V1(time).Create(),
+			new P3V2(time).Create(),
+		};
 
 		var kept = new HashSet<string>
 		{
@@ -16,17 +19,22 @@ public static class Program
 			BookwormCharacters.Benno.Name,
 			BookwormCharacters.Lutz.Name
 		};
-		foreach (var key in p3v1.Points.Keys.ToList())
+		foreach (var book in books)
 		{
-			if (!kept.Contains(key))
+			foreach (var key in book.Points.Keys.ToList())
 			{
-				//p3v1.Points.Remove(key);
+				if (!kept.Contains(key))
+				{
+					//p3v1.Points.Remove(key);
+				}
 			}
 		}
 
 		const string DIR = @"C:\Users\User\Downloads";
-		p3v1.Export(DIR);
-		p3v2.Export(DIR);
+		foreach (var book in books)
+		{
+			book.Export(DIR);
+		}
 
 		Console.ReadLine();
 	}
