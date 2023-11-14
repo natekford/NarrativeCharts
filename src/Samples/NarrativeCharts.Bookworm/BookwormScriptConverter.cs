6a63423035f0277085b7ff094617dc13868a3978
Advorange <advorange@live.com>
2023-11-13 21:59:27 -0800

    BookwormScriptConverter's output looks nicer.

--- src/Samples/NarrativeCharts.Bookworm/BookwormScriptConverter.cs
+++ src/Samples/NarrativeCharts.Bookworm/BookwormScriptConverter.cs
@@ -3,6 +3,7 @@ using NarrativeCharts.Scripting;
 using NarrativeCharts.Time;
 
 using System.Reflection;
+using System.Text;
 
 namespace NarrativeCharts.Bookworm;
 
@@ -20,6 +21,8 @@ public sealed class BookwormScriptConverter : ScriptConverter
 
 	public string Write()
 	{
+		var nl = Environment.NewLine;
+
 		var methods = new Dictionary<string, string>();
 		for (var i = 0; i < Chapters.Count; ++i)
 		{
@@ -28,9 +31,8 @@ public sealed class BookwormScriptConverter : ScriptConverter
 $@"
 	private void {name}()
 	{{
-{Chapters[i]}
-	}}
-";
+		{Chapters[i].Replace(nl, $"{nl}\t\t").TrimEnd()}
+	}}";
 			methods.Add($"{name}();", method);
 		}
 
@@ -42,10 +44,9 @@ $@"
 			properties.Add($"private {TYPE} {property} {{ get; set; }} = null!;");
 		}
 
-		// the string has bad formatting, but the ide will fix that
+		// god this is so ugly
 		return
-$@"
-using static {nameof(NarrativeCharts)}.{nameof(Bookworm)}.{nameof(BookwormBell)};
+$@"using static {nameof(NarrativeCharts)}.{nameof(Bookworm)}.{nameof(BookwormBell)};
 using static {nameof(NarrativeCharts)}.{nameof(Bookworm)}.{nameof(BookwormCharacters)};
 using static {nameof(NarrativeCharts)}.{nameof(Bookworm)}.{nameof(BookwormLocations)};
 
@@ -53,7 +54,7 @@ namespace {nameof(NarrativeCharts)}.{nameof(Bookworm)}.*;
 
 public sealed class {ClassName} : {nameof(BookwormNarrativeChart)}
 {{
-	{string.Join($"{Environment.NewLine}\t", properties)}
+	{string.Join($"{nl}\t", properties)}
 
 	public {ClassName}({nameof(TimeTrackerWithUnits)} time) : base(time)
 	{{
@@ -62,12 +63,11 @@ public sealed class {ClassName} : {nameof(BookwormNarrativeChart)}
 
 	protected override void {nameof(ProtectedCreate)}()
 	{{
-		{string.Join($"{Environment.NewLine}\t\t", methods.Keys)}
+		{string.Join($"{nl}\t\t", methods.Keys)}
 		{nameof(Update)}();
 	}}
-	{string.Join("\t", methods.Values)}
-}}
-";
+	{string.Join($"{nl}\t", methods.Values)}
+}}";
 	}
 
 	protected override IEnumerable<string> ToProperties(IEnumerable<Character> characters)
@@ -87,5 +87,34 @@ public sealed class {ClassName} : {nameof(BookwormNarrativeChart)}
 			.GetProperties(BindingFlags.Static | BindingFlags.Public)
 			.Where(x => x.PropertyType == typeof(T))
 			.ToDictionary(x => getValue((T)x.GetValue(null)!), x => x.Name);
+	}
+}
+
+internal static class BookwormScriptConverterUtils
+{
+	// copied from https://stackoverflow.com/a/24769702
+	public static StringBuilder? TrimEnd(this StringBuilder sb)
+	{
+		if (sb == null || sb.Length == 0)
+		{
+			return sb;
+		}
+
+		var i = sb.Length - 1;
+
+		for (; i >= 0; i--)
+		{
+			if (!char.IsWhiteSpace(sb[i]))
+			{
+				break;
+			}
+		}
+
+		if (i < sb.Length - 1)
+		{
+			sb.Length = i + 1;
+		}
+
+		return sb;
 	}
 }
