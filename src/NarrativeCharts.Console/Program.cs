1b75de15cc1eb3794b70f7e14a0e30184a0c0ede
natekford <nkeelanford@gmail.com>
2023-12-15 01:30:57 -0800

    Inverted some conditionals in NarrativeCharts.Console.Program so there was less indentation and made some comments cleaerer.

--- src/NarrativeCharts.Console/Program.cs
+++ src/NarrativeCharts.Console/Program.cs
@@ -24,94 +24,91 @@ public class Program(ImmutableArray<string> Args)
 	{
 		var directory = GetDirectory();
 		await ProcessAsync(directory, null).ConfigureAwait(false);
-
-		if (!IsCmd)
+		if (IsCmd)
 		{
-			while (true)
+			return;
+		}
+
+		while (true)
+		{
+			var ticks = Interlocked.Exchange(ref _Changed, DEFAULT_CHANGED);
+			if (ticks == DEFAULT_CHANGED)
 			{
-				var ticks = Interlocked.Exchange(ref _Changed, DEFAULT_CHANGED);
-				if (ticks != DEFAULT_CHANGED)
-				{
-					// remove 1 second from the time we're using because of minor
-					// tick differences between what FSW event can get and what
-					// the file actually has as its timestamp
-					// it was only like .01 seconds difference but i dont think
-					// making it 1 second difference matters much
-					ticks -= TICKS_PER_SECOND;
-					try
-					{
-						// always call this instead of reusing scripts/defs/drawer
-						// b/c scripts/defs are things that can be externally changed
-						await ProcessAsync(directory, new(ticks)).ConfigureAwait(false);
-					}
-					catch (Exception e)
-					{
-						System.Console.WriteLine(e);
-					}
-				}
-				else
-				{
-					// only need this delay if processasync isn't called because
-					// process async takes more than 250ms by itself usually
-					await Task.Delay(250).ConfigureAwait(false);
-				}
+				// No scripts/definitions have been edited, keep waiting and
+				// periodically check until something has been edited
+				await Task.Delay(250).ConfigureAwait(false);
+				continue;
 			}
+
+			// There's a minor tick difference between File.LastWriteTimeUtc and
+			// the times we get from FSW.Changed (espeically because the event
+			// doesn't provide any times itself so we have to use DateTime.UtcNow)
+			// It's a ~10ms difference, so subtracting 1s is more than enough
+			ticks -= TICKS_PER_SECOND;
+			// Don't reuse any script definitions or parsed scripts because they
+			// can be externally edited
+			await ProcessAsync(directory, new(ticks)).ConfigureAwait(false);
 		}
 	}
 
 	private static async Task ProcessAsync(string directory, DateTime? comparisonTimeUtc)
 	{
-		var defsPath = Path.Combine(directory, "ScriptDefinitions.json");
-		var defs = await ScriptDefinitions.LoadAsync(defsPath).ConfigureAwait(false);
-		defs.ComparisonTimeUtc = comparisonTimeUtc;
-		var scripts = defs.LoadScripts().ToList();
-		// todo: put drawer properties into ScriptDefinitions
-		var drawer = new SKChartDrawer()
+		try
 		{
-			ImageAspectRatio = 16f / 9f,
-			CharacterLabelColorConverter = SKColorConverters.Color(SKColors.Black),
-		};
+			var defsPath = Path.Combine(directory, "ScriptDefinitions.json");
+			var defs = await ScriptDefinitions.LoadAsync(defsPath).ConfigureAwait(false);
+			defs.ComparisonTimeUtc = comparisonTimeUtc;
+			var scripts = defs.LoadScripts().ToList();
+			// todo: put drawer properties into ScriptDefinitions
+			var drawer = new SKChartDrawer()
+			{
+				ImageAspectRatio = 16f / 9f,
+				CharacterLabelColorConverter = SKColorConverters.Color(SKColors.Black),
+			};
 
-		await defs.ProcessAsync(scripts, drawer).ConfigureAwait(false);
+			await defs.ProcessAsync(scripts, drawer).ConfigureAwait(false);
+		}
+		catch (Exception e)
+		{
+			System.Console.WriteLine(e);
+		}
 	}
 
 	private string GetDirectory()
 	{
 		if (IsCmd)
 		{
-			var directory = Args.SingleOrDefault();
-			if (!Directory.Exists(directory))
+			var argDirectory = Args.SingleOrDefault();
+			if (!Directory.Exists(argDirectory))
 			{
 				throw new ArgumentException("Directory does not exist.");
 			}
-			return directory;
+			return argDirectory;
 		}
-		else
+
+		string? directory;
+		do
 		{
-			string? directory;
-			do
-			{
-				System.Console.WriteLine("Enter a directory to parse scripts from: ");
-				directory = System.Console.ReadLine();
-			}
-			while (!Directory.Exists(directory));
-
-			// just use fsw as an indicator that something was changed, we don't care
-			// about what was changed, and trying to do the async processing in
-			// the Changed event causes a lot of problems
-			_FileSystemWatcher = new FileSystemWatcher()
-			{
-				Path = directory,
-				EnableRaisingEvents = true,
-				Filter = "*.*",
-			};
-			_FileSystemWatcher.Changed += OnChanged;
-
-			System.Console.WriteLine($"Started watching {directory} for changes.");
-			System.Console.WriteLine();
-
-			return directory;
+			System.Console.WriteLine("Enter a directory to parse scripts from: ");
+			directory = System.Console.ReadLine();
 		}
+		while (!Directory.Exists(directory));
+
+		// just use fsw as an indicator that something was changed, we don't care
+		// about what was changed, and trying to do the async processing in
+		// the Changed event causes a lot of problems
+		_FileSystemWatcher = new FileSystemWatcher()
+		{
+			Path = directory,
+			EnableRaisingEvents = true,
+			Filter = "*.*",
+		};
+		_FileSystemWatcher.Changed += OnChanged;
+
+		System.Console.WriteLine($"Started watching {directory} for changes.");
+		System.Console.WriteLine();
+
+		return directory;
 	}
 
 	private void OnChanged(object sender, FileSystemEventArgs e)
